6f5ec0d439b2ffa73019b7c5f3b744c908932184
CyborgVillager <almawijonathan@gmail.com>
2019-09-29 13:25:43 -0500

    update

---
+++ notes/distancetraveled.py
@@ -0,0 +1,7 @@
+#vehicle travel calc
+#distance = speed * time
+
+vehicle_speed = float(input('What is your vehicle speed?'))
+time = float(input('How many hours are you driving for?'))
+distance = vehicle_speed * time
+print('your distance is ' , distance)
